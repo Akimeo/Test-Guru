f70b956bf36a190c7870754b07d628e719abcda9
Akimeo <akim.parinoff@gmail.com>
2021-05-19 02:24:02 +0300

    Associations set

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,13 @@
 # frozen_string_literal: true
 
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User'
+
+  has_many :questions
+  has_many :viewed_tests
+  has_many :users, through: :viewed_tests
+
   def self.sort_by_category(category)
     Test
       .joins('JOIN categories ON tests.category_id = categories.id')
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :viewed_tests
+  has_many :tests, through: :viewed_tests
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+
   def tests_by_difficulty(level)
     Test
       .joins('JOIN viewed_tests ON tests.id = viewed_tests.test_id')
