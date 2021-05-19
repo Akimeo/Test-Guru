4316dac7043e3724bcff9764adc7fa03bcbc986c
Akimeo <akim.parinoff@gmail.com>
2021-05-19 21:09:07 +0300

    Dependences added

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,13 +4,13 @@ class Test < ApplicationRecord
   belongs_to :category
   belongs_to :author, class_name: 'User'
 
-  has_many :questions
-  has_many :viewed_tests
+  has_many :questions, dependent: :destroy
+  has_many :viewed_tests, dependent: :destroy
   has_many :users, through: :viewed_tests
 
   def self.sort_by_category(category)
     Test
-      .joins('JOIN categories ON tests.category_id = categories.id')
+      .joins(:category)
       .where('categories.title = ?', category)
       .order('tests.title DESC')
       .pluck(:title)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,11 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  has_many :viewed_tests
+  has_many :viewed_tests, dependent: :destroy
   has_many :tests, through: :viewed_tests
-  has_many :created_tests, class_name: 'Test', foreign_key: :author_id
+  has_many :created_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  def tests_by_difficulty(level)
-    Test
-      .joins('JOIN viewed_tests ON tests.id = viewed_tests.test_id')
-      .where('user_id = ? AND level = ?', id, level)
+  def tests_by_level(level)
+    tests.where('level = ?', level)
   end
 end
