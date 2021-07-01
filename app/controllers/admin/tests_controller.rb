290fce2a05283e0d33b89ac846fd06d1b7a4c062
Akimeo <akim.parinoff@gmail.com>
2021-07-01 13:02:41 +0300

    Small changes

--- app/controllers/admin/tests_controller.rb
+++ app/controllers/admin/tests_controller.rb
@@ -1,6 +1,5 @@
 class Admin::TestsController < Admin::BaseController
   before_action :find_test, only: %i[show edit update destroy]
-  before_action :find_user, only: %i[new create]
 
   def index
     @tests = Test.all
@@ -10,11 +9,11 @@ class Admin::TestsController < Admin::BaseController
   end
 
   def new
-    @test = @user.created_tests.new
+    @test = current_user.created_tests.new
   end
 
   def create
-    @test = @user.created_tests.new(test_params)
+    @test = current_user.created_tests.new(test_params)
 
     if @test.save
       redirect_to admin_test_path(@test)
@@ -43,10 +42,6 @@ class Admin::TestsController < Admin::BaseController
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = current_user
   end
 
   def test_params
