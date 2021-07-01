c0d854da022df6f74f507ba5265bcccaecf6fbf5
Akimeo <akim.parinoff@gmail.com>
2021-07-01 12:53:34 +0300

    Callback removed

--- app/controllers/tests_controller.rb
+++ app/controllers/tests_controller.rb
@@ -1,23 +1,18 @@
 class TestsController < ApplicationController
   before_action :find_test, only: :start
-  before_action :find_user, only: :start
 
   def index
     @tests = Test.all
   end
 
   def start
-    @user.tests.push(@test)
-    redirect_to @user.viewed_test(@test)
+    current_user.tests.push(@test)
+    redirect_to current_user.viewed_test(@test)
   end
 
   private
 
   def find_test
     @test = Test.find(params[:id])
-  end
-
-  def find_user
-    @user = current_user
   end
 end
